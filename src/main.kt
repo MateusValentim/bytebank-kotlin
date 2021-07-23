5431d4a3f0840f31c61e78dbaa405ec31b3adcf1
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-07-22 22:50:43 -0300

    adicionando classe funcionario

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,16 @@
 fun main() {
     println("Bem vindo ao Bytebank:")
 
-    testaComportamentosConta()
+    var mateus = Funcionario(
+        cpf = "233.222.765.42",
+        nome = "Mateus Valentim",
+        salario = 1000.0
+    )
+
+    println("nome: ${mateus.nome}")
+    println("cpf: ${mateus.cpf}")
+    println("salario: ${mateus.salario}")
+    print("bonificação: ${mateus.bonificacao()}")
 }
 
 
