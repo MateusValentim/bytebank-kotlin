723b153f0da03140f2714bd8db1b2784cce392fd
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-07-23 00:07:40 -0300

    adicionando classe Gerente

--- src/Funcionario.kt
+++ src/Funcionario.kt
@@ -1,11 +1,24 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: Int // 0 - funcionario, 1 - gerente, 2 - diretor
 ) {
 
-    fun bonificacao(): Double{
-        return salario * 0.1
+    fun bonificacao(): Double {
+        when (tipo) {
+            0 -> {
+                return salario * 0.1
+            }
+            else -> {
+                return salario * 0.3
+            }
+        }
     }
-
 }
+
+
+
+
+
+
--- src/main.kt
+++ src/main.kt
@@ -1,16 +1,38 @@
 fun main() {
     println("Bem vindo ao Bytebank:")
+    println()
 
     var mateus = Funcionario(
         cpf = "233.222.765.42",
         nome = "Mateus Valentim",
-        salario = 1000.0
+        salario = 1000.0,
+        tipo = 2
+    )
+
+    var helena = Gerente(
+        cpf = "111.111.111-11",
+        nome = "Helena Regina Valentim",
+        salario = 5000.0,
+        senha = 1111
     )
 
     println("nome: ${mateus.nome}")
     println("cpf: ${mateus.cpf}")
     println("salario: ${mateus.salario}")
-    print("bonificação: ${mateus.bonificacao()}")
+    println("bonificação: ${mateus.bonificacao()}")
+
+    println()
+
+    println("nome: ${helena.nome}")
+    println("cpf: ${helena.cpf}")
+    println("salario: ${helena.salario}")
+    println("bonificação: ${helena.bonificacao()}")
+
+    if (helena.autentica(1111)){
+        println("Autenticado com sucesso")
+    } else{
+        println("Falha na autenticação")
+    }
 }
 
 
