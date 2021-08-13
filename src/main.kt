16745860cc2654e36c11402cc1cf2a65ad08565e
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-08-12 22:07:01 -0300

    arquivo testaAutenticacao

--- src/main.kt
+++ src/main.kt
@@ -1,24 +1,6 @@
 fun main() {
-    val gerente: FuncionarioAdmin = Gerente(
-        nome = "Mateus",
-        senha = 1234,
-        salario = 2000.0,
-        cpf = "123.123.123.21"
-    )
-    val diretor: FuncionarioAdmin =
-        Diretor(
-            nome = "Helena",
-            senha = 1234,
-            salario = 3000.0,
-            cpf = "222.222.222.22",
-            plr = 1000.0
-        )
-
-    val  sistemaInterno = SistemaInterno ()
-    sistemaInterno.entra(gerente, senha = 1234)
-    sistemaInterno.entra(diretor, senha = 1234)
+    testaAutenticacao()
 }
-
 
 
 
