88461a562bc1c144d7865034ef7eb9e8a13445c4
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-08-30 23:12:14 -0300

    exemplo de object expression

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,27 @@
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaCorrente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 
 
 fun main() {
+//object expression
+    //Cria um objeto diretamente no arquivo sem a necessidade de criar uma classe
+
+    val thalia = object : Autenticavel{
+        val nome : String = "Thalia"
+        val cpf: String = "111.111.111.11"
+        val senha: Int = 1234
+
+        override fun autentica(senha: Int) = this.senha == senha
+
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(thalia, 1234)
+    println("nome do cliente: ${thalia.nome}, ${thalia.cpf}")
+
     val mateus : Cliente = Cliente(nome = "Mateus Valentim", "", senha = 1)
     val contaPoupanca = ContaPoupanca(titular = mateus, numeroConta = 1)
     val contaCorrente = ContaCorrente(titular = mateus, numeroConta = 1001)
-    
+
     testaContasDiferentes()
 
     println("Total de contas: ${totalContas}")
