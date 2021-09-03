63660af7de875945dbce6e77d70114245b8e1db3
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-09-02 23:23:03 -0300

    Trabalhando com o tratamento de excessões

--- src/main.kt
+++ src/main.kt
@@ -1,22 +1,40 @@
 import br.com.alura.bytebank.modelo.*
+import java.lang.ArithmeticException
+import java.lang.ClassCastException
 
 
 fun main() {
-    val endereco = Endereco(logradouro = "Rua Vitória", cep = "10100011")
-    val enderecoNovo = Endereco(logradouro = "Rua Vitória",cep = "10100011")
-
-    println(endereco)
-    println(enderecoNovo)
-
-    println(endereco.equals(enderecoNovo))
-    println(enderecoNovo.hashCode())
-    println(endereco.hashCode())
-
-}
-    fun imprime(valor: Any) : Any {
-        println(valor)
-        return valor
+    println("Inicio main")
+    try {
+        10/0
+    } catch ( e :ArithmeticException){
+        println("ArithmeticException foi capturada, por favor divida por algum número válido")
     }
+    funcao1()
+    println("Fim main")
+}
+
+fun funcao1(){
+    println("Inicio função 1")
+    try {
+        funcao2()
+    } catch (e : ClassCastException){
+        println("ClassCastException foi capturada")
+    }
+    println("Fim função 1")
+}
+
+fun funcao2(){
+println("Inicio função 2")
+    for (i in 1..5){
+        println(i)
+        //Exception de cast
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("Fim função 2")
+}
+
 
 
 
