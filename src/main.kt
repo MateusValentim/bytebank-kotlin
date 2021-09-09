c1bb19c59ccec6f9561e7d1337beb154e2f9b684
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-09-09 11:44:18 -0300

    Utilizando o ElvisOperator e SafeCast

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,27 @@
 import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.modelo.Endereco
+import java.lang.IllegalStateException
 
 
 fun main() {
 
-var enderecoNulo: Endereco? = Endereco(logradouro = "Rua vergueiro")
-val novoLogradouro: String? = enderecoNulo?.logradouro
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua vergueiro")
+    val novoLogradouro: String? = enderecoNulo?.logradouro
 
     enderecoNulo?.let {
         println(it.logradouro.length)
+        //Elvis Operator
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento tem que ser preenchido")
+        println(tamanhoComplemento)
     }
+    teste("")
+    teste(1)
+}
+//Safe Casts
+//Faz um cast para tipos nullable, ou podem receber nulo
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int
 }
 
 
