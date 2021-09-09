f32a22beeaf820a3841b80476be5370c7bb2d4c2
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-09-09 00:07:45 -0300

    Tipo que podem receber nulo

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,12 @@
 import br.com.alura.bytebank.exception.SaldoInsuficienteException
+import br.com.alura.bytebank.modelo.Endereco
 
 
 fun main() {
-    println("Inicio main")
-    testaComportamentosConta()
-    println("Fim main")
+
+    val endereco : Endereco? = null
+    val enderecoNaoNulo : Endereco = endereco!!
+    enderecoNaoNulo.logradouro
 }
 
 
